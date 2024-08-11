6032a85c23f40d771a1a53ef509c29c2dbcd5346
aquilaneo <aquilaneo@gmail.com>
2024-08-11 16:07:15 +0900

    biome導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
-  return (
-    <>
-      <h1>タイトル</h1>
-    </>
-  )
+	return (
+		<>
+			<h1>タイトル</h1>
+		</>
+	);
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const targetElem = document.getElementById("root");
+if (targetElem !== null) {
+	createRoot(targetElem).render(
+		<StrictMode>
+			<App />
+		</StrictMode>,
+	);
+}
