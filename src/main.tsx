1d1501bc6ecac987de00d09ca6fea9f9c068021a
aquilaneo <aquilaneo@gmail.com>
2024-08-12 16:57:57 +0900

    グローバルスタイル適用

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,23 @@
-import { Global, css } from "@emotion/react";
+/** @jsxImportSource @emotion/react */
+import { css, Global } from "@emotion/react";
 import emotionReset from "emotion-reset";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
+// グローバルスタイル
+const globalStyle = css(emotionReset, {
+	body: {
+		fontFamily:
+			'"Helvetica Neue", Arial, "Hiragino Kaku Gothic ProN", "Hiragino Sans", Meiryo, sans-serif',
+	},
+});
+
 const targetElem = document.getElementById("root");
 if (targetElem !== null) {
 	createRoot(targetElem).render(
 		<>
-			<Global styles={css(emotionReset)} />
+			<Global styles={globalStyle} />
 			<StrictMode>
 				<App />
 			</StrictMode>
